9cbd9540390164d1804da4d3b73b138479ae95cc
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-29 19:11:58 -0300

    (feat): página de login criada e estilizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home(){
-  return(
-    <div>
-      <h1>Página Home</h1>
-    </div>
-  )
+import login from '@/assets/login-background.png'
+import logo from '@/assets/Logo.svg'
+import Image from 'next/image';
+import style from './styles.module.css'
+import LoginInput from '@/components/LoginInput';
+
+export default function LoginPage() {
+    
+
+    return(
+        <>
+            <div className={style.box}>
+                <div className={style.background}>
+                    <Image src={login} alt="foto de livros e uma luminária" /> 
+                </div>
+                <div className={style.login}>
+                    <Image src={logo} alt='logo'/>
+
+                    <p>Bem vindo(a)!</p>
+                    <h2>Entre na sua conta</h2>
+
+                    <LoginInput/>
+                    
+                </div>
+            </div>
+        </>
+    )
 }
