11b71aa6a9868b15b0a8394c9098b065d455a877
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-29 19:06:09 -0300

    (feat): componente do card do livro da Home Page criado e estilizado

---
+++ src/components/CardGenero/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useNavigate } from 'react-router-dom';
+import CardLivro from '@/components/CardLivro';
+import style from './styles.module.css';
+import { Livro } from '@/types/Livro';
+import { useRouter } from 'next/navigation';
+
+interface CardGeneroProps {
+    genero: string;
+    livros: Livro[];
+}
+
+export default function CardGenero({ genero, livros }: CardGeneroProps) {
+
+    const router = useRouter();
+
+    function handleClick() {
+        router.push(`/home/${genero}`)
+    }
+
+    const livrosFiltrados = livros.filter(livro => livro.genero === genero).slice(0,4)
+
+    return (
+        <>
+            <div className={style.card}>
+                <div className={style.about}>
+                   <h1>{genero}</h1>
+                    <p onClick={handleClick}>Ver mais</p> 
+                </div>
+                <div className={style.livros}>
+                    {livrosFiltrados.map(livro => (
+                        <CardLivro key={livro.id} livro={livro}/>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
